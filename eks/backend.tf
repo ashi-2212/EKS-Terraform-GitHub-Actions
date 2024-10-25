7b573c0ddec8eff86afb97e0b2ca5573b42490c9
ashi-2212 <ashi.singh@appinventiv.com>
2024-10-25 15:29:09 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-backet1"
+    bucket         = "sample-project-bucket22"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
